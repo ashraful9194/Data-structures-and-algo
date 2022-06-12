ef0d48ab91290a99b2592f76135dcc7ac8b41156
MD Ashraful Alam <ashraful9194@gmail.com>
2022-06-12 16:24:28 +0600

    Add files via upload

---
+++ Number of Digits of n!.cpp
@@ -0,0 +1,11 @@
+ /**
+    Formulas :
+
+    Number of digits of a number n = log(n)+1;
+    Number of digits of  n! = log(1*2*3*4*.......*n)+1;
+                            =log(1)+log(2)+log(3)+........log(n)+1;
+    So, in base b logb(n!)=log(n!)*logb(10)+1;
+                          =(log(n!)/log(b))+1
+
+    log is 10 base.....
+    */
